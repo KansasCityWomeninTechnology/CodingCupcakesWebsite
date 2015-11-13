0785a6b6b2ec0eb3d215e41c554a0ab35acf0053
Jennifer Wadella <jennifer.wadella@gmail.com>
2015-11-13 10:13:04 -0600

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+// $( document ).ready(function() {
+//     $('#navbar a').click(function(e) {
+//     	e.preventDefault();
+//     	var target = $(this).attr('href');
+//     	$('#code').scrollTo({
+// 		    duration: 2000,
+// 		    easing: 'linear'
+// 		});
+//     });
+// });
+
