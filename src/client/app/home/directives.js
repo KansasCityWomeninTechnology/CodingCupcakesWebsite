a7a56404dfb52dd9162707f970f80119ab698fba
Sarah <sksmart9@gmail.com>
2019-11-08 20:10:29 -0600

    Site cleanup * Add leadership team * Remove outdated mentors * Add 2020 session dates * Update session time for November 2019 * Remove 2019 sessions already completed * Remove .DS_Store files

--- src/client/app/home/directives.js
+++ src/client/app/home/directives.js
@@ -1,46 +1,46 @@
 function hero() {
    return {
       template: require('./partials/hero.html')
-   }
-};
+   };
+}
 
 function thecode() {
    return {
       template: require('./partials/code.html'),
       controller: 'codeCtrl as cvm'
-  }
-};
+  };
+}
 
 function thecupcakes() {
    return {
       template: require('./partials/cupcakes.html')
-   }
-};
+   };
+}
 
 function mentors() {
    return {
       template: require('./partials/mentors.html'),
       controller: 'mentorCtrl as mvm'
-   }
-};
+   };
+}
 
 function signup() {
    return {
       template: require('./partials/signup.html')
-   }
-};
+   };
+}
 
 function contact() {
    return {
       template: require('./partials/contact.html')
-   }
-};
+   };
+}
 
 function press() {
    return {
       template: require('./partials/press.html')
-   }
-};
+   };
+}
 
 export default angular.module('home.directives', [])
    .directive('hero', hero)
